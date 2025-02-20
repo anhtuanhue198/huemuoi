914f6e3ffe56859def14dd6fda10c7e58474df5f
anhtuanhue198 <123873744+anhtuanhue198@users.noreply.github.com>
2025-02-20 19:10:48 +0000

    Xoá file tên_file

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", function () {
-    let nav = document.getElementById("site-header");
-
-    if (window.scrollY > 50) {
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
-});
