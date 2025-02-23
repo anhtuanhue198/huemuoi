3f8b06ef85ae6ab6c5b8441613f2fe66f02c9a07
anhtuanhue198 <123873744+anhtuanhue198@users.noreply.github.com>
2025-02-23 14:03:45 +0000

    Thêm file tên_file

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-// document.addEventListener("scroll", function () {
-//     let nav = document.getElementById("site-header");
-
-//     if (window.scrollY > 50) {
-//         nav.classList.add("scrolled");
-//     } else {
-//         nav.classList.remove("scrolled");
-//     }
-// });
-// Lắng nghe sự kiện khi bấm vào link
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault(); // Ngừng hành động mặc định của link
-
-      // Thực hiện cuộn đến phần tử có id tương ứng
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",  // Chuyển đến với hiệu ứng mượt mà
-        block: "start"       // Đưa phần tử đến vị trí đầu của section
-      });
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("site-header");
+  if (window.scrollY > 50) { // Khi kéo xuống 50px
+      header.classList.add("scrolled");
+  } else {
+      header.classList.remove("scrolled");
+  }
+});
+ // Hàm di chuyển đến vị trí của section và lùi lại 90px
+ function scrollToElement(elementId) {
+  const element = document.querySelector(elementId);  // Lấy phần tử cần scroll đến
+  if (element) {
+    window.scrollTo({
+      top: element.offsetTop - 100,  // Lùi lại 90px
+      behavior: 'smooth'  // Thêm hiệu ứng cuộn mượt mà
     });
+  }
+}
+
+// Lắng nghe sự kiện click của các link
+document.querySelectorAll('.hero-buttons a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();  // Ngăn chặn hành động mặc định của link (di chuyển ngay lập tức)
+    const target = this.getAttribute('href');  // Lấy href của link
+    scrollToElement(target);  // Gọi hàm cuộn đến vị trí
   });
+});
+
 
